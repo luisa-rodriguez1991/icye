f77b3fcd1250caf323812335b320d0b92788d43a
Luisa-Rodriguez <lrodriguezm1991@gmail.com>
2023-10-24 16:42:33 -0500

    show all correction

--- components/calendar-events.tsx
+++ components/calendar-events.tsx
@@ -115,14 +115,14 @@ const days = [
 //   return classes.filter(Boolean).join(' ')
 // }
 
-export default function Calendar({showAll}) {
+export default function Calendar({showAll}:any) {
 
   const getFirst3Meetings = meetings.filter((meeting) => (meeting.id < 4) )
 
   const first3 = getFirst3Meetings.map((meeting) =>  (
 
     <li key={meeting.id} className="relative flex space-x-6 py-6 xl:static bg-accent-2 my-2">
-      
+
             <img src={meeting.imageUrl} alt="" className="h-14 w-14 flex-none rounded-full mx-4" />
             <div className="flex-auto ">
               <h3 className="pr-10 font-semibold text-gray-900 xl:pr-0">{meeting.name}</h3>
