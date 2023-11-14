3f38edcebb3b03ba7be483d9c18e4ee890203d00
Luisa-Rodriguez <lrodriguezm1991@gmail.com>
2023-11-14 17:51:41 -0500

    any blog

--- components/list-blog.tsx
+++ components/list-blog.tsx
@@ -4,7 +4,7 @@ import { useRouter } from "next/router";
 export default function Blog({ text, pageInfo, posts, showAll }: any) {
   const router = useRouter();
   const { locale } = router;
-  const all = posts.map((post) => (
+  const all = posts.map((post:any) => (
     <article
       key={post.slug}
       className="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl bg-gray-900 px-8 pb-8 pt-80 sm:pt-48 lg:pt-80"
