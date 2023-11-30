4e1eaacf6a88527d04ca36631e464cdbcc699e9c
Fabian Haeckermann <fhaeckermann-ua@immoweltag.de>
2023-11-30 17:18:39 +0100

    Fix Form

--- components/form.tsx
+++ components/form.tsx
@@ -444,7 +444,7 @@ export default function Form({ openIndicator, onSetOpen }: any) {
                                   <div className="mt-2.5">
                                     <input
                                       type="text"
-                                      {...register("Phone", { maxLength: 20 })}
+                                      {...register("phone", { maxLength: 20 })}
                                       aria-describedby="phone-description"
                                       className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                     />
@@ -538,7 +538,7 @@ export default function Form({ openIndicator, onSetOpen }: any) {
                                   </div>
                                   <div className="mt-2.5">
                                     <textarea
-                                      {...register("Message", {})}
+                                      {...register("message", {})}
                                       rows={4}
                                       className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                     />
