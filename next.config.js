56ac7e5675fb2537fd4262d3894dab7fbc5ade0f
Fabian Haeckermann <fhaeckermann-ua@immoweltag.de>
2023-12-08 15:50:43 +0100

    Fix search

--- next.config.js
+++ next.config.js
@@ -8,10 +8,10 @@ const nextConfig = {
   i18n: {
     // These are all the locales you want to support in
     // your application
-    locales: ["default", "es", "en"],
+    locales: ["es", "en"],
     // This is the default locale you want to be used when visiting
     // a non-locale prefixed path e.g. `/hello`
-    defaultLocale: 'default',
+    defaultLocale: 'es',
     localeDetection: false,
   },
 }
