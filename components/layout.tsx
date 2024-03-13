60e76d56f8a1aeb5d9d90696f9ead8e843f4a438
Luisa-Rodriguez <lrodriguezm1991@gmail.com>
2024-03-13 13:23:34 -0500

    analytics

--- components/layout.tsx
+++ components/layout.tsx
@@ -2,6 +2,7 @@ import Footer from "./footer";
 import Meta from "./meta";
 import Header from "./header";
 import Form from "./form";
+import Script from 'next/script';
 
 export default function Layout({
   children,
@@ -11,7 +12,25 @@ export default function Layout({
 }: any) {
   return (
     <>
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-TQ9M4CN6M5"/>
+    <Script
+      id='google-analytics'
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-TQ9M4CN6M5', {
+            page_path: window.location.pathname,
+          });
+        `,
+        }}
+    />
+
+     
       <Meta />
+
       <div className="min-h-screen bg-white">
         <Form openIndicator={openForm} onSetOpen={onOpenForm} />
         <Header onOpenForm={onOpenForm} />
